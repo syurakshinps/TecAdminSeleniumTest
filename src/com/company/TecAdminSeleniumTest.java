844d6b1a7d9c89af2d29c549bd57f04ab4b9be5e
Pavel Syu <poet.poet@gmail.com>
2022-03-27 09:57:18 +0300

    searches for catfood. no captcha yet

--- src/com/company/TecAdminSeleniumTest.java
+++ src/com/company/TecAdminSeleniumTest.java
@@ -1,20 +1,24 @@
 package com.company;
+import java.io.File;
 import java.io.IOException;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+/*
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+*/
 public class TecAdminSeleniumTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--no-sandbox");
-        chromeOptions.addArguments("log-level=2");
+      //  chromeOptions.addArguments("--headless");
+     //   chromeOptions.addArguments("--no-sandbox");
+        //   chromeOptions.addArguments("log-level=2");
         WebDriver driver = new ChromeDriver(chromeOptions);
 /*
         driver.get("https://google.com");
@@ -29,20 +33,39 @@ public class TecAdminSeleniumTest {
 
         */
 String baseURL = "http://market.yandex.ru";
-String catalogueButton = "/html/body/div[1]/div[3]/noindex/div/div/div[2]/div[1]/div/div/button";
-String catFood = "/html/body/div[1]/div[3]/div[1]/noindex/div/div/div[2]/div[2]/div/div/form";
-String elementToClickToFind = "/html/body/div[1]/div[3]/div[1]/noindex/" +
-        "div/div/div[2]/div[2]/div/div/form/div/button/span";
+String catalogueButton = "/html/body/div[1]/div[3]/noindex/div/div/div[2]/div[1]/div/div/button/span/div/div/div[2]";
+String searchInputField = "//html/body/div[1]/div[3]/noindex/div/div/div[2]/div[2]/div/div/form/div[1]/div/div/div/input";
+String searchText ="кошачий корм";
+String clickToFind = "/html/body/div[1]/div[3]/noindex/div/div/div[2]/div[2]/div/div/form/div[1]/button/span";
 
 
 
         driver.get(baseURL);
+        if (driver.getPageSource().contains("робот"))
+        {
+            System.out.println("яндекс хилые и трусливые говнюки");
+   //          https://www.browserstack.com/guide/how-to-handle-captcha-in-selenium
+        }
 
-//        driver.get(catalogueButton);
+        Thread.sleep(30000);
+
         WebElement catalogue = driver.findElement(By.xpath(catalogueButton));
         catalogue.click();
+        Thread.sleep(10000);
+
+        driver.findElement(By.xpath(searchInputField)).sendKeys(searchText);
+        Thread.sleep(10000);
+
+        WebElement clickable = driver.findElement(By.xpath(clickToFind));
+        clickable.click();
+        Thread.sleep(10000);
 
 
+
+        /*
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+       FileUtils.copyFile(scrFile, new File("src\\test\\resources\\screenshot.png"));
+*/
         driver.quit();
     }
 }
