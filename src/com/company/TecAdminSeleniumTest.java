1983d3b5f89a1463e39281321317ede531b9e3d2
Pavel Syu <poet.poet@gmail.com>
2022-03-27 19:34:07 +0300

    all ok but captcha (out of the scope)

--- src/com/company/TecAdminSeleniumTest.java
+++ src/com/company/TecAdminSeleniumTest.java
@@ -47,6 +47,9 @@ String treatsForCatsKind = "/html/body/div[21]/div[3]/div[1]/div/div/div/div/div
 String treatsURL = "https://market.yandex.ru/catalog--lakomstva-dlia-koshek/62819/list?hid=15963668";
 String deliveryServiceRadioBtn ="/html/body/div[1]/div[3]/div[3]/div[5]/aside/div[2]/div/div/div/div/div[2]/div[20]/div/div/fieldset/ul/li[1]/div/label/div";
 String makerName = "/html/body/div[1]/div[3]/div[3]/div[5]/aside/div[2]/div/div/div/div/div[2]/div[4]/div/div/fieldset/ul/li[11]/div/a/label/div";
+
+String firstBestChoice = "/html/body/div[1]/div[3]/div[3]/div[5]/div/div[2]/div/div[1]/div/div/div/div/div/article[1]/div[3]/a";
+
         driver.get(baseURL);
         if (driver.getPageSource().contains("робот"))
         {
@@ -91,7 +94,7 @@ String makerName = "/html/body/div[1]/div[3]/div[3]/div[5]/aside/div[2]/div/div/
 
         driver.findElement(By.xpath(priceFromField)).sendKeys(priceMin);
         System.out.println("min price " + priceMin);
-        Thread.sleep(10000);
+        // Thread.sleep(10000);
 
 
         driver.findElement(By.xpath(priceToFiled)).sendKeys(priceMax);
@@ -108,7 +111,10 @@ String makerName = "/html/body/div[1]/div[3]/div[3]/div[5]/aside/div[2]/div/div/
         System.out.println("Деревенские лакомства - маректолога потом уволили, надеюсь");
         Thread.sleep(10000);
 
-
+        WebElement choice = driver.findElement(By.xpath(firstBestChoice));
+        choice.click();
+        System.out.println("Первый в списке под популярными");
+        Thread.sleep(10000);
 
 
 
